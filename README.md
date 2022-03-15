0d2a71cd34dd0be848ddf47bc09af242a91d9025
Uselysses <andrew.lawrence.donald@gmail.com>
2022-03-15 15:22:20 -0700

    Adds annuity calculations, readme additions

--- README.md
+++ README.md
@@ -18,10 +18,10 @@ or
 
 # Usage
 
-## Present Value:
+## Present Value - Lump Sum:
 
 ```javascript
-import {presentValue, presentValueAnswer} from 'mortgage-quant';
+import { presentValue } from 'mortgage-quant';
 
 const futureValue = 200_000; 
 const annualInterestRate = 0.01; 
@@ -34,39 +34,39 @@ console.log(myPresentValue);
 ```
 
 
-## About Present Value
+## About Present Value - Lump Sum
 Present value is the current value of money you expect in the future, given a 
 specified rate of return and time period (specifically in months for this 
 function).
 
 The purpose of any present value calculation is to determine the dollar amount
-it would take to reach a future value, over a period of time, with a specific
-interest rate.
+it would take to reach a future value with a one-time investment, 
+over a period of time, with a specific interest rate.
 
 ---
 
 ## Future Value:
 
 ```javascript
-import {futureValue, futureValueAnswer} from 'mortgage-quant';
+import { futureValue } from 'mortgage-quant';
 
 const presentValue = 148_384.58355742485697957747; 
 const annualInterestRate = 0.01;
 const numberOfYears = 30;
 
-const myFutureValue = futureValue(presentValue, annualInterestRate, years);
+const myFutureValue = futureValue(presentValue, annualInterestRate, numberOfYears);
 
 console.log(myFutureValue);
 // returns 200000.000000000004071073225607124555368865704...
 
 ```
-## About Future Value:
-Future value is the yield of any investment made today, given a present value,
-an annual interest rate, and the number of years you intend to invest. 
+## About Future Value - Lump Sum:
+Future value is the yield of any flat investment made today, given a present 
+value (lump investment), an annual interest rate, and the number of years you 
+intend to invest. 
 
 As we can see both present and future value functions output their reverse with 
-miniscule inaccuracies. The reasons for these minute inaccuracies is explained 
-below.
+miniscule inaccuracies. The reasons for these inaccuracies is explained below.
 
 
 
@@ -93,4 +93,71 @@ to base-10, please refer to their documentation for implementation instructions.
 
 Without a tool like BigNumber, our functions above would be dramatically 
 inaccurate. 
+
+## Present Value of an Annuity
+
+```javascript
+import { presentValueAnnuity } from 'mortgage-quant';
+
+const payment = 7_406; 
+const interestRate = 0.01;
+const numberOfPayments = 360;
+
+const myPresentValueAnnuity = presentValueAnnuity(payment, interestRate, numberOfPayments);
+
+console.log(myPresentValueAnnuity);
+// returns 
+
+```
+
+# About Present Value of an Annuity
+
+While the previous present value function dealt with lump sums, present value 
+of an annuity deals with a continuous stream of payments.
+
+In the context of mortgages, present value of an annuity gives us the lender's 
+perspective: will the return from the stream of payments be higher than the 
+present amount that I'm lending?
+
+With respect to the time value of money, present cash is more valuable than the 
+same amount paid over time, given the opportunity cost of not investing it 
+elsewhere.
+
+This equation allows lenders to check the viability of a mortgage; what a 
+stream of mortgage payments will eventually return over time, and to show the 
+investor whether the price they're paying is above or below expected value.
+
+In simple terms, the present value of an annuity is the value of all future 
+annuity payments. It calculates the present value of future cash flows.
+
+Given a monthly payment, an interest rate, and the number of payments to be 
+made, the present value of an annuity will output your total loan amount.
+
+
+## Future Value of an Annuity
+
+
+
+
+## Monthly Payment
+
+```javascript
+import { monthlyPayment } from 'mortgage-quant';
+
+const principal = 720_000; 
+const interestRate = 0.01;
+const numberOfPayments = 360;
+
+const myMonthlyPayment = monthlyPayment(principal, interestRate, numberOfPayments);
+
+console.log(myMonthlyPayment);
+// returns 
+
+```
+
+The purpose of this function is to determine your monthly payment given a 
+mortgage amount, monthly compounding interest rate, and number of payments over 
+the course of the mortgage.
+
+
 
--- index.js
+++ index.js
@@ -6,12 +6,9 @@ import BigNumber from "bignumber.js";
 // You can check out bankroll for an implementation example in ruby:
 // https://github.com/nolantait/bankroll/blob/master/lib/bankroll/present_value.rb
 
-// Things you might want to look up to help you:
-// - Annuity factor
-// - Mortgage factor
-// - Fixed annuities
-// - Floating point math in Javascript
 
+
+// PRESENT VALUE OF MONEY:
 
 export const presentValue = (annualInterestRate, numberOfYears, futureValue) => {
   futureValue = BigNumber(futureValue);
@@ -20,11 +17,15 @@ export const presentValue = (annualInterestRate, numberOfYears, futureValue) => 
   return futureValue.dividedBy((annualInterestRate.plus(1)).exponentiatedBy(numberOfYears));
 };
 
-const presentValueAnswer = presentValue(0.01, 30, 200_000);
+const presentValueAnswer = presentValue(0.05, 15, 1_000_000);
 
 console.log('Present Value:', presentValueAnswer.toString());
 
 
+
+
+
+// FUTURE VALUE OF MONEY:
 
 const futureValue = (annualInterestRate, numberOfYears, presentValue) => {
   annualInterestRate = BigNumber(annualInterestRate);
@@ -40,34 +41,94 @@ console.log('Future Value:', futureValueAnswer.toString());
 
 
 
-// I'm buying a home, putting down 50k down payment, 500k loan, 4% interest rate, over 30 years
+// Present Value of an Annuity
 
-// $773,536.02
+const presentValueAnnuity = (payment, interestRate, numberOfPayments) => {
+  payment = BigNumber(payment);
+  interestRate = BigNumber(interestRate);
+  numberOfPayments = BigNumber(numberOfPayments);
 
-// payment arguments - necessary
-
-
-// FutureValue over N periods =
-// periodicPayment[(1 + interestRate) ** periods - 1] / interestRate
-
-const futureValueOfPayments = (annualInterestRate, numberOfPayPeriods,  periodicPayment) => {
-  annualInterestRate = BigNumber(annualInterestRate);
-  numberOfPayPeriods = BigNumber(numberOfPayPeriods);
-  periodicPayment = BigNumber(periodicPayment);
-
-  return periodicPayment.times(((annualInterestRate.plus(1)).exponentiatedBy(numberOfPayPeriods).minus(1)).dividedBy(annualInterestRate));
+  const compoundInterest =
+    interestRate
+      .plus(1)
+      .exponentiatedBy(numberOfPayments);
+  
+  const numerator = 1 - (1 / (compoundInterest));
+  
+  return payment.times(numerator / interestRate);
 };
 
-const futureValuePaymentsAnswer = futureValueOfPayments(0.05, 5, 1_000);
+const presentAnnuityAnswer = presentValueAnnuity(7_406, 0.01, 360);
+
+console.log('Present Value of an Annuity:', presentAnnuityAnswer.toString());
+
+
+
+// FUTURE VALUE OF AN ANNUITY:
+
+const futureValueAnnuity = (payment, interestRate, numberOfPayments) => {
+  payment = BigNumber(payment);
+  interestRate = BigNumber(interestRate);
+  numberOfPayments = BigNumber(numberOfPayments);
+
+  const compoundInterest =
+    interestRate
+      .plus(1)
+      .exponentiatedBy(numberOfPayments);
+  
+  return payment.times((compoundInterest.minus(1)).dividedBy(interestRate));
+
+};
+
+const futureAnnuityAnswer = futureValueAnnuity(125_000, 0.08, 5);
+
+console.log('Future Value of an Annuity:', futureAnnuityAnswer.toString());
+
+
+// FUTURE VALUE OF PAYMENTS:
+
+const futureValueOfPayments = (interestRate, numberOfPayments, payment, presentValue) => {
+  interestRate = BigNumber(interestRate);
+  numberOfPayments = BigNumber(numberOfPayments);
+  payment = BigNumber(payment);
+  presentValue = BigNumber(presentValue);
+
+  const compoundInterest =
+    interestRate
+      .plus(1)
+      .exponentiatedBy(numberOfPayments);
+  
+  const simpleFutureValue = 
+    payment
+      .times(compoundInterest.minus(1))
+      .dividedBy(interestRate);
+  
+  const interestPaid =
+    presentValue.times(compoundInterest);
+  
+  
+  return interestPaid.plus(simpleFutureValue);
+};
+
+const temp = BigNumber(0.1).dividedBy(12);
+
+const futureValuePaymentsAnswer = futureValueOfPayments(temp, 360, 100, 10_000);
 
 console.log('Future Value of Payments:', futureValuePaymentsAnswer.toString());
 
 
-// Verified the above with examples, gives the future value of payments but isn't what Nolan has in his ruby code, doesn't output relevant answer
 
 
-// TRANSLATING THE RUBY CODE:
+// Verified the above with examples, gives the future value of payments but isn't what Nolan has in his ruby code, doesn't output relevant answer because it doesn't contain PRESENT VALUE OR PAYMENT AMOUNTS
 
+
+
+
+
+
+// FUTURE TOTAL COST OF A FIXED RATE MORTGAGE
+// Monthly payments * # of months 
+// doesn't account for variable rates
 
 
 const futureValueMortgage = (numberOfPayPeriods, annualInterestRate, periodicPayment, newPresentValue) => {
@@ -78,11 +139,66 @@ const futureValueMortgage = (numberOfPayPeriods, annualInterestRate, periodicPay
 
   let effectiveRate = (annualInterestRate.plus(1)).exponentiatedBy(numberOfPayPeriods);
 
-  effectiveRate = BigNumber(effectiveRate);
-
   return (effectiveRate.times(newPresentValue)).plus(periodicPayment.times((annualInterestRate.times(0)).plus(1)).times(effectiveRate.minus(1)).dividedBy(annualInterestRate));
 };
 
-const futureValueMortgageAnswer = futureValueMortgage(30, 0.04, 2_148, 450_000);
+const futureValueMortgageAnswer = futureValueMortgage(360, 0.0033333, 2_148, 450_000);
 
 console.log('Future Value Mortgage:', futureValueMortgageAnswer.toString());
+
+
+
+// MONTHLY PAYMENT:
+
+const monthlyPayment = (principal, interestRate, numberOfPayments) => {
+  principal = BigNumber(principal);
+  interestRate = BigNumber(interestRate);
+  numberOfPayments = BigNumber(numberOfPayments);
+
+  let compoundInterest =
+    interestRate
+      .plus(1)
+      .exponentiatedBy(numberOfPayments);
+  
+  let numerator =
+    interestRate
+      .times(principal)
+      .times(compoundInterest);
+
+  let denominator = compoundInterest.minus(1);
+
+  return numerator.dividedBy(denominator);
+};
+
+const monthlyPaymentAnswer = monthlyPayment(720_000, 0.01, 360);
+
+console.log('Monthly Payment:', monthlyPaymentAnswer.toString());
+
+
+
+// AMORTIZATION
+// Create some sort of map, array or chart with the following columns/arguments:
+    // Month, Beginning Balance, Interest, Principal, Payment Amount, Ending Balance, Equity
+    // Payments will be heavily weighted towards paying off interest at the start
+      // What proportion? How is the weighting calculated?
+    // The flow of the schedule:
+      // Month 1 | Beginning Balance = 200,000 | Interest Rate 6%/12 = 0.005 | Monthly Payment = 16,687.71
+      // Apply the .005 interest rate to the balance, which is 1,000: subtract this from the monthly payment
+      // Monthly Payment - Monthly Interest = Principal Payment (687.71)
+      // We then subtract Principal Payment from the balance = 199,312.29
+      // We then loop it, applying the interest rate to the new balance, and printing out the row values for each month
+        // Every month the interest gets lower, and the amount paid to principal gets higher, as well as their equity in the home
+  // Early Payoff, needs flexibility
+
+
+const amortizationPrincipal = (monthlyPayment, outstandingBalance, interestRate) => {
+  monthlyPayment = BigNumber(monthlyPayment);
+  outstandingBalance = BigNumber(outstandingBalance);
+  interestRate = BigNumber(interestRate);
+
+  return monthlyPayment.minus(outstandingBalance.times(interestRate.dividedBy(12)));
+};
+
+const amortPrincipalAnswer = amortizationPrincipal(2_415.70, 450_000, 0.05);
+
+console.log('Monthly Principal Due:', amortPrincipalAnswer.toString());
